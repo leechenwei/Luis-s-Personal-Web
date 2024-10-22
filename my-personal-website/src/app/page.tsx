a0b9ccd2ae9007a6e90b0e009fbb975537dd20e2
Luis <78288358+leechenwei@users.noreply.github.com>
2024-10-22 22:42:13 +0800

    Profile Update

--- my-personal-website/src/app/page.tsx
+++ my-personal-website/src/app/page.tsx
@@ -1,9 +1,16 @@
-import Portfolio from '@/components/Portfolio'
+import Head from 'next/head';
+import Portfolio from '@/components/Portfolio';
 
 export default function Home() {
   return (
-    <main>
-      <Portfolio />
-    </main>
-  )
+    <>
+      <Head>
+        <title>Luis's Personal Web</title>
+        <meta name="description" content="Luis Lee Chen Wei's Portfolio" />
+      </Head>
+      <main>
+        <Portfolio />
+      </main>
+    </>
+  );
 }
